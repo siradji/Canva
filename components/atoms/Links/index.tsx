b0b4bf3a2a09a0885b18b0f393bb2eb332d003c7
Siradji <siradjiawoual@gmail.com>
2021-03-01 00:38:36 +0000

    Atom components button and links

---
+++ components/atoms/Links/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+interface LinkProp {
+    readonly size: string,
+    readonly colour: string,
+    readonly w: string
+}
+
+interface Iprops extends LinkProp {
+    children: React.ReactNode
+}
+
+
+const index: React.FC<Iprops> = ({size, colour, w, children}: Iprops) => {
+    return (
+        <Link colour={colour} size={size} w={w}>
+            {children}
+        </Link>
+    )
+}
+
+const Link = styled.a<LinkProp>`
+    color: #${props => props.colour};
+    font-size: ${props => props.size}em;
+    font-weight: ${props => props.w};
+    display: flex;
+    align-items: center;
+`
+
+export default index
